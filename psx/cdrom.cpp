53847164783289c2ad3875075707fa45ad635b46
Jan Böker <jan.boeker@googlemail.com>
2025-02-25 08:05:41 +0100

    Implement CDROM interrupt registers

--- psx/cdrom.cpp
+++ psx/cdrom.cpp
@@ -3,34 +3,102 @@
 #include <cassert>
 #include <format>
 
+#include "exceptions/exceptions.h"
 #include "util/log.h"
 
 using namespace util;
 
 namespace PSX {
 
+CDROM::CDROM() {
+    reset();
+}
+
 void CDROM::reset() {
+    index = 0;
 }
 
-template <typename T>
-void CDROM::write(uint32_t address, T value) {
-    assert ((address >= 0x1F801800) && (address < 0x1F801800 + sizeof(T)));
-    LOG_CDROM(std::format("CDROM unimplemented: write 0x{:0{}X} -> @0x{:08X}", value, 2*sizeof(T), address));
+uint8_t CDROM::getStatusRegister() {
+    return (0 << CDROMSTAT_BUSYSTS)
+           || (0 << CDROMSTAT_DRQSTS)
+           || (0 << CDROMSTAT_RSLRRDY)
+           || (1 << CDROMSTAT_PRMWRDY)
+           || (1 << CDROMSTAT_PRMEMPT)
+           || (0 << CDROMSTAT_ADPBUSY)
+           || ((index & 3) << CDROMSTAT_INDEX0);
 }
 
-template void CDROM::write(uint32_t address, uint32_t value);
-template void CDROM::write(uint32_t address, uint16_t value);
-template void CDROM::write(uint32_t address, uint8_t value);
-
-template <typename T>
-T CDROM::read(uint32_t address) {
-    LOG_CDROM(std::format("CDROM unimplemented: read @0x{:08X}", address));
-    return (1 << 3);
+template <>
+void CDROM::write(uint32_t address, uint32_t value) {
+    throw exceptions::UnimplementedAddressingError(std::format("word write @0x{:08X}", address));
 }
 
-template uint32_t CDROM::read(uint32_t address);
-template uint16_t CDROM::read(uint32_t address);
-template uint8_t CDROM::read(uint32_t address);
+template <>
+void CDROM::write(uint32_t address, uint16_t value) {
+    throw exceptions::UnimplementedAddressingError(std::format("half-word write @0x{:08X}", address));
+}
+
+template <>
+void CDROM::write(uint32_t address, uint8_t value) {
+    assert ((address >= 0x1F801800) && (address < 0x1F801804));
+
+    if (address == 0x1F801800) { // status register
+        LOG_CDROM(std::format("Write to status register: 0x{:0{}X}", value, 2*sizeof(value)));
+        index = value & 3; // only the index can be written to
+
+    } else if (address == 0x1F801802) {
+        if (index == 1) { // interrupt enable register
+            LOG_CDROM(std::format("Write to interrupt enable register: 0x{:0{}X}", value, 2*sizeof(value)));
+            interruptEnableRegister = value & 0x1F; // bits 5--7 are unused
+
+        } else {
+            LOG_CDROM(std::format("Unimplemented write 0x{:0{}X} -> @0x{:08X}, index {:d}", value, 2*sizeof(value), address, index));
+        }
+
+    } else if (address == 0x1F801803) {
+        if (index == 1) { // interrupt flag register
+            LOG_CDROM(std::format("Write to interrupt flag register: 0x{:0{}X}", value, 2*sizeof(value)));
+            // Writing 1 to bits 0--4 resets them
+            interruptFlagRegister = interruptFlagRegister & ~(value & 0x1F);
+
+            // TODO Writing 1 to bit 6 resets parameter queue
+
+        } else {
+            LOG_CDROM(std::format("Unimplemented write 0x{:0{}X} -> @0x{:08X}, index {:d}", value, 2*sizeof(value), address, index));
+        }
+
+    } else {
+        LOG_CDROM(std::format("Unimplemented write 0x{:0{}X} -> @0x{:08X}, index {:d}", value, 2*sizeof(value), address, index));
+    }
+}
+
+template <>
+uint32_t CDROM::read(uint32_t address) {
+    throw exceptions::UnimplementedAddressingError(std::format("word read @0x{:08X}", address));
+}
+
+template <>
+uint16_t CDROM::read(uint32_t address) {
+    throw exceptions::UnimplementedAddressingError(std::format("half-word read @0x{:08X}", address));
+}
+
+template <>
+uint8_t CDROM::read(uint32_t address) {
+    assert ((address >= 0x1F801800) && (address < 0x1F801804));
+
+    uint8_t value = 0;
+
+    if (address == 0x1F801800) { // status register
+        value = getStatusRegister();
+
+        LOG_CDROM(std::format("Read from status register: 0x{:0{}X}", value, 2*sizeof(value)));
+
+    } else {
+        LOG_CDROM(std::format("Unimplemented read @0x{:08X}, index {:d}", address, index));
+    }
+
+    return value;
+}
 
 }
 
--- psx/cdrom.h
+++ psx/cdrom.h
@@ -5,11 +5,46 @@
 
 namespace PSX {
 
+// 1F801800h - Index/Status Register (Bit0-1 R/W) (Bit2-7 Read Only)
+// 7 BUSYSTS Command/parameter transmission busy  (1=Busy)
+#define CDROMSTAT_BUSYSTS 7
+// 6 DRQSTS  Data fifo empty      (0=Empty) ;triggered after reading LAST byte
+#define CDROMSTAT_DRQSTS  6
+// 5 RSLRRDY Response fifo empty  (0=Empty) ;triggered after reading LAST byte
+#define CDROMSTAT_RSLRRDY 5
+// 4 PRMWRDY Parameter fifo full  (0=Full)  ;triggered after writing 16 bytes
+#define CDROMSTAT_PRMWRDY 4
+// 3 PRMEMPT Parameter fifo empty (1=Empty) ;triggered before writing 1st byte
+#define CDROMSTAT_PRMEMPT 3
+// 2 ADPBUSY XA-ADPCM fifo empty  (0=Empty) ;set when playing XA-ADPCM sound
+#define CDROMSTAT_ADPBUSY 2
+// 0-1 Index   Port 1F801801h-1F801803h index (0..3 = Index0..Index3)   (R/W)
+#define CDROMSTAT_INDEX1  1
+#define CDROMSTAT_INDEX0  0
+
 class CDROM {
 private:
+    // Status Register
+    // 0x1F801800
+    // index is part of the status register
+    uint8_t index;
+
+    // Interrupt Enable Register
+    // 0x1F801802, index 1 write
+    // 0x1F801803, index 0 read
+    // 0x1F801803, index 2 read
+    uint8_t interruptEnableRegister;
+
+    // Interrupt Flag Register
+    // 0x1F801803, index 1 read and write
+    // 0x1F801803, index 3 read
+    uint8_t interruptFlagRegister;
 
 public:
+    CDROM();
     void reset();
+
+    uint8_t getStatusRegister();
 
     template <typename T>
     void write(uint32_t address, T value);
