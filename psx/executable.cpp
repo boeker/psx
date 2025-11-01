39c10197d97d0b3500a437e4962970239a12c743
Jan Böker <jan.boeker@googlemail.com>
2025-11-01 19:30:34 +0100

    Fix read across memory boundary

--- psx/executable.cpp
+++ psx/executable.cpp
@@ -99,7 +99,7 @@ void Executable::parseHeader() {
 void Executable::writeToMemory() {
     // Copy to RAM
     assert(header.fileSize <= exeLength + 0x800);
-    for (uint32_t i = 0; i <= exeLength; ++i) {
+    for (uint32_t i = 0; i <= header.fileSize; ++i) {
         bus->write<uint8_t>(header.destination + i, exe[0x800 + i]);
     }
 
