337f131100b65b7d40a6e53f5b0f203ede9f2dcd
Jan Böker <jan.boeker@googlemail.com>
2026-01-18 07:10:15 +0100

    Add helper functions for operations on bits

--- psx/util/bit.h
+++ psx/util/bit.h
@@ -9,6 +9,28 @@ namespace Bit {
     inline uint16_t extendSign11To16(uint16_t value) {
         return ((value >> 10) ? 0xF800 : 0x0000) + value;
     }
+
+    template<typename T>
+    inline bool getBit(T target, uint8_t bit) {
+        return target & (1 << bit);
+    }
+
+    template<typename T>
+    inline void setBit(T &target, uint8_t bit, bool value) {
+        uint32_t selectedBit = 1 << bit;
+        target = (target & ~selectedBit) | ((value ? 1 : 0) << bit);
+    }
+
+    template<typename T>
+    inline void setBit(T &target, uint8_t bit) {
+        target = target | (1 << bit);
+    }
+
+    template<typename T>
+    inline void clearBit(T &target, uint8_t bit) {
+        uint32_t selectedBit = 1 << bit;
+        target = target & ~selectedBit;
+    }
 };
 
 }
