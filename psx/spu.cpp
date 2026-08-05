53a642380094fb20715e33164c6d06fda4bc90e0
Jan Böker <jan.boeker@googlemail.com>
2026-08-05 11:40:09 +0200

    Implement basic access logic for SPU control registers

--- psx/spu.cpp
+++ psx/spu.cpp
@@ -1,35 +1,159 @@
 #include "spu.h"
 
 #include <cassert>
+#include <cstring>
 #include <format>
 
 #include "util/log.h"
+#include "exceptions/exceptions.h"
 
 using namespace util;
 
 namespace PSX {
 
+SPU::SPU()
+    : ram(std::make_unique<uint8_t[]>(SPU_RAM_SIZE)) {
+    reset();
+}
+
 void SPU::reset() {
+    std::memset(ram.get(), 0, SPU_RAM_SIZE);
+
+    irq_address_register = 0;
+    data_transfer_address_register = 0;
+    control_register = 0;
+    data_transfer_control_register = 0;
+    status_register = 0;
 }
 
-template <typename T>
-void SPU::write(uint32_t address, T value) {
-    LOGV_SPU(std::format("SPU unimplemented: write @0x{:08X}", address));
+void SPU::handle_control_write(uint32_t address, uint16_t value) {
+    assert(address >= 0x1F80'1DA0 && address < 0x1F80'1DBF);
+    assert((address & 1) == 0);
+
+    uint32_t offset = address & 0x0000'00FF;
+    if (offset == 0x0A4) {
+        LOGW_SPU(std::format("Unimplemented write to RAM IRQ Address @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xA6) {
+        LOGW_SPU(std::format("Unimplemented write to RAM Data Transfer Address @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xA8) {
+        LOGW_SPU(std::format("Unimplemented write to RAM Data Transfer Queue @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAA) {
+        LOGW_SPU(std::format("Unimplemented write to Control Register @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAC) {
+        LOGW_SPU(std::format("Unimplemented write to RAM Data Transfer Control @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAE) {
+        LOGW_SPU(std::format("Unimplemented write to Status Register @0x{:08X}", address));
+        // TODO
+    } else {
+        LOGW_SPU(std::format("Write to unknown control register @0x{:08X}", address));
+    }
 }
 
-template void SPU::write(uint32_t address, uint32_t value);
-template void SPU::write(uint32_t address, uint16_t value);
-template void SPU::write(uint32_t address, uint8_t value);
+uint16_t SPU::handle_control_read(uint32_t address) {
+    assert(address >= 0x1F80'1DA0 && address < 0x1F80'1DBF);
+    assert((address & 1) == 0);
 
-template <typename T>
-T SPU::read(uint32_t address) {
-    LOGV_SPU(std::format("SPU unimplemented: read @0x{:08X}", address));
+    uint32_t offset = address & 0x0000'00FF;
+    if (offset == 0x0A4) {
+        LOGW_SPU(std::format("Unimplemented read from RAM IRQ Address @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xA6) {
+        LOGW_SPU(std::format("Unimplemented read from RAM Data Transfer Address @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xA8) {
+        LOGW_SPU(std::format("Unimplemented read from RAM Data Transfer Queue @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAA) {
+        LOGW_SPU(std::format("Unimplemented read from Control Register @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAC) {
+        LOGW_SPU(std::format("Unimplemented read from RAM Data Transfer Control @0x{:08X}", address));
+        // TODO
+    } else if (offset == 0xAE) {
+        LOGW_SPU(std::format("Unimplemented read from Status Register @0x{:08X}", address));
+        // TODO
+    } else {
+        LOGW_SPU(std::format("Read from unknown control register @0x{:08X}", address));
+    }
+
     return 0;
 }
 
-template uint32_t SPU::read(uint32_t address);
-template uint16_t SPU::read(uint32_t address);
-template uint8_t SPU::read(uint32_t address);
+template <>
+void SPU::write(uint32_t address, uint32_t value) {
+    throw exceptions::UnimplementedAddressingError(std::format("SPU: word write @0x{:08X}", address));
+}
+
+template <>
+void SPU::write(uint32_t address, uint16_t value) {
+    assert(address >= 0x1F80'1C00 && address < 0x1F80'1FFF);
+    assert((address & 1) == 0); // Only aligned writes supported for now
+
+    uint32_t offset = address & 0x0000'FFFF;
+    if (offset < 0x1D7F) {
+        LOGW_SPU(std::format("Unimplemented write to voice register @0x{:08X}", address));
+    } else if (offset < 0x1D87) {
+        LOGW_SPU(std::format("Unimplemented write to volume register @0x{:08X}", address));
+    } else if (offset < 0x1D9F) {
+        LOGW_SPU(std::format("Unimplemented write to voice flags @0x{:08X}", address));
+    } else if (offset < 0x1DBF) {
+        handle_control_write(address, value);
+    } else if (offset < 0x1DFF) {
+        LOGW_SPU(std::format("Unimplemented write to reverb configuration area @0x{:08X}", address));
+    } else if (offset < 0x1E5F) {
+        LOGW_SPU(std::format("Unimplemented write to internal voice registers @0x{:08X}", address));
+    } else if (offset < 0x1FFF) {
+        LOGW_SPU(std::format("Unimplemented write to unknown area @0x{:08X}", address));
+    }
+}
+
+template <>
+void SPU::write(uint32_t address, uint8_t value) {
+    throw exceptions::UnimplementedAddressingError(std::format("SPU: byte write @0x{:08X}", address));
+}
+
+template <>
+uint32_t SPU::read(uint32_t address) {
+    throw exceptions::UnimplementedAddressingError(std::format("SPU: word read @0x{:08X}", address));
+    return 0;
+}
+
+template <>
+uint16_t SPU::read(uint32_t address) {
+    assert(address >= 0x1F80'1C00 && address < 0x1F80'1FFF);
+    assert((address & 1) == 0); // Only aligned reads supported for now
+
+    uint16_t value = 0;
+    uint32_t offset = address & 0x0000'FFFF;
+    if (offset < 0x1D7F) {
+        LOGW_SPU(std::format("Unimplemented read from voice register @0x{:08X}", address));
+    } else if (offset < 0x1D87) {
+        LOGW_SPU(std::format("Unimplemented read from volume register @0x{:08X}", address));
+    } else if (offset < 0x1D9F) {
+        LOGW_SPU(std::format("Unimplemented read from voice flags @0x{:08X}", address));
+    } else if (offset < 0x1DBF) {
+        value = handle_control_read(address);
+    } else if (offset < 0x1DFF) {
+        LOGW_SPU(std::format("Unimplemented read from reverb configuration area @0x{:08X}", address));
+    } else if (offset < 0x1E5F) {
+        LOGW_SPU(std::format("Unimplemented read from internal voice registers @0x{:08X}", address));
+    } else if (offset < 0x1FFF) {
+        LOGW_SPU(std::format("Unimplemented read from unknown area @0x{:08X}", address));
+    }
+
+    return value;
+}
+
+template <>
+uint8_t SPU::read(uint32_t address) {
+    throw exceptions::UnimplementedAddressingError(std::format("SPU: byte read @0x{:08X}", address));
+    return 0;
+}
 
 }
 
--- psx/spu.h
+++ psx/spu.h
@@ -2,14 +2,66 @@
 #define PSX_SPU_H
 
 #include <cstdint>
+#include <memory>
+
+#define SPU_RAM_SIZE (512 * 1024)
+
+#define SPU_CONTROL_ENABLE 15 // 0 = off, 1 = on
+#define SPU_CONTROL_MUTE 14 // 0 = mute, 1 = unmute
+#define SPU_CONTROL_NOISE_FREQUENCY_SHIFT3 13 //0,...,15 = Low Frequency,...,High Frequency
+#define SPU_CONTROL_NOISE_FREQUENCY_SHIFT2 12
+#define SPU_CONTROL_NOISE_FREQUENCY_SHIFT1 11
+#define SPU_CONTROL_NOISE_FREQUENCY_SHIFT0 10
+#define SPU_CONTROL_NOISE_FREQUENCY_STEP1 9 // 0,...,3 = Step 4,...,7
+#define SPU_CONTROL_NOISE_FREQUENCY_STEP0 8
+#define SPU_CONTROL_REVERB_MASTER_ENABLE 7 // 0 = disabled, 1 = enabled
+#define SPU_CONTROL_IRQ9_ENABLE 6 // Read: 0 = disabled, 1 = enabled (if SPU_CONTROL_ENABLE is set), Write: 0 = Acknowledge
+#define SPU_CONTROL_RAM_TRANSFER_MODE1 5 // 0 = stop, 1 = manual write,
+#define SPU_CONTROL_RAM_TRANSFER_MODE0 4 // 2 = DMA write to SPU RAM, 3 = DMA read from SPU RAM
+#define SPU_CONTROL_EXTERNAL_AUDIO_REVERB 3 // 0 = off, 1 = on
+#define SPU_CONTROL_CD_AUDIO_REVERB 2 // 0 = off, 1 = on
+#define SPU_CONTROL_EXTERNAL_AUDIO_ENABLE 1 // 0 = off, 1 = on
+#define SPU_CONTROL_CD_AUDIO_ENABLE 0 // 0 = off, 1 = on
+
+#define SPU_STATUS_CAPTURE_HALF 11 // 0 = write to first half, 1 = write to second half
+#define SPU_STATUS_TRANSFER_BUSY 10 // 0 = ready, 1 = busy
+#define SPU_STATUS_TRANSFER_READ_REQUEST 9 // 0 = no, 1 = yes
+#define SPU_STATUS_TRANSFER_WRITE_REQUEST 8 // 0 = no, 1 = yes
+#define SPU_STATUS_TRANSFER_READ_WRITE_REQUEST 7 // 0 = no, 1 = yes, this is SPU_CONTROL_RAM_TRANSFER_MODE1
+#define SPU_STATUS_IRQ9_FLAG 6 // 0 = no, 1 = interrupt request
+#define SPU_STATUS_RAM_TRANSFER_MODE1 5 // cf. SPU_CONTROL
+#define SPU_STATUS_RAM_TRANSFER_MODE0 4
+#define SPU_STATUS_EXTERNAL_AUDIO_REVERB 3 // cf. SPU_CONTROL
+#define SPU_STATUS_CD_AUDIO_REVERB 2 // cf. SPU_CONTROL
+#define SPU_STATUS_EXTERNAL_AUDIO_ENABLE 1 // cf. SPU_CONTROL
+#define SPU_STATUS_CD_AUDIO_ENABLE 0 // cf. SPU_CONTROL
+
 
 namespace PSX {
 
 class SPU {
 private:
+    std::unique_ptr<uint8_t[]> ram;
+
+    // 0x1F80'1DA4: SPU RAM IRQ Address (halfword)
+    uint16_t irq_address_register;
+    // 0x1F80'1DA6: SPU RAM Data Transfer Address (divided by 8, halfword)
+    uint16_t data_transfer_address_register;
+    // 0x1F80'1DA8: SPU RAM Data Transfer Queue (stores up to 32 halfwords)
+    // TODO
+    // 0x1F80'1DAA: SPU Control Register (SPUCNT)
+    uint16_t control_register;
+    // 0x1F80'1DAC: SPU RAM Data Transfer Control (halfword, should be 0x0004)
+    uint16_t data_transfer_control_register;
+    // 0x1F80'1DAE: SPU Status Register (SPUSTAT)
+    uint16_t status_register;
 
 public:
+    SPU();
     void reset();
+
+    void handle_control_write(uint32_t address, uint16_t value);
+    uint16_t handle_control_read(uint32_t address);
 
     template <typename T>
     void write(uint32_t address, T value);
